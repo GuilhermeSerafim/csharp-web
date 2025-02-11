75ee1f85376d55b371cfec3bcb0701f3ab7eb231
GuilhermeSerafim <guilerstudies@gmail.com>
2025-02-10 23:34:08 -0200

    Implementando banco com entity framework

--- ScreenSound/Banco/MusicaDAL.cs
+++ ScreenSound/Banco/MusicaDAL.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ScreenSound.Modelos;
 
 namespace ScreenSound.Banco;
 
 public class MusicaDAL
 {
+    private readonly ScreenSoundContext _context;
+
+    public MusicaDAL(ScreenSoundContext context)
+    {
+        _context = context;
+    }
+
+    public IEnumerable<Musica> Listar()
+    {
+        return new List<Musica>();
+    }
+
+    public void Adicionar(Musica musica)
+    {
+        _context.Musica.Add(musica);
+        _context.SaveChanges();
+    }
+
+    public void Deletar(Musica musica)
+    {
+        _context.Musica.Remove(musica);
+        _context.SaveChanges();
+    }
 
 }
